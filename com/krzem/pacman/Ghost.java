042febb77dde8a01bdcb5704de5371b7dce8c547
Krzem <50489938+Krzem5@users.noreply.github.com>
2020-03-28 16:37:54 +0100

    Push Update

---
+++ com/krzem/pacman/Ghost.java
@@ -0,0 +1,35 @@
+package com.krzem.pacman;
+
+
+
+import java.awt.Graphics2D;
+
+
+
+public class Ghost extends Constants{
+	public Main cls;
+	public Board b;
+	public int x;
+	public int y;
+
+
+
+	public Ghost(Main cls,Board b,int x,int y){
+		this.cls=cls;
+		this.b=b;
+		this.x=x;
+		this.y=y;
+	}
+
+
+
+	public void update(){
+
+	}
+
+
+
+	public void draw(Graphics2D g){
+
+	}
+}
