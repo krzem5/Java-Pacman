659b708e5f1b0f9768bff85251f68e997bf9eb5a
Krzem <krzem5.dev@gmail.com>
2021-01-12 20:21:26 +0100

    Push Update 01/12/2021, 20:03:43

--- index.bat
+++ index.bat
@@ -1,5 +1,21 @@
-echo off
-echo NUL>_.class&&del /s /f /q *.class
+@echo off
 cls
-javac com/krzem/pacman/Main.java&&java com/krzem/pacman/Main
-start /min cmd /c "echo NUL>_.class&&del /s /f /q *.class"
+if exist build rmdir /s /q build
+mkdir build
+cd src
+javac -d ../build com/krzem/pacman/Main.java&&jar cvmf ../manifest.mf ../build/pacman.jar -C ../build *&&goto run
+cd ..
+goto end
+:run
+cd ..
+pushd "build"
+for /D %%D in ("*") do (
+	rd /S /Q "%%~D"
+)
+for %%F in ("*") do (
+	if /I not "%%~nxF"=="pacman.jar" del "%%~F"
+)
+popd
+cls
+java -jar build/pacman.jar
+:end
