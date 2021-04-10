c67f071c44d6c15974da1b1e357b9079ad64dc1b
Krzem <krzem5.dev@gmail.com>
2021-04-10 21:11:56 +0200

    Push Update 04/10/2021, 21:11:49

--- build.bat
+++ build.bat
@@ -1,21 +1,3 @@
 @echo off
 cls
-if exist build rmdir /s /q build
-mkdir build
-cd src
-javac -d ../build com/krzem/pacman/Main.java&&jar cvmf ../manifest.mf ../build/pacman.jar -C ../build *&&goto run
-cd ..
-goto end
-:run
-cd ..
-pushd "build"
-for /D %%D in ("*") do (
-	rd /S /Q "%%~D"
-)
-for %%F in ("*") do (
-	if /I not "%%~nxF"=="pacman.jar" del "%%~F"
-)
-popd
-cls
-java -jar build/pacman.jar
-:end
+python build.py %*
